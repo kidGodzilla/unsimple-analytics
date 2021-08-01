c249c5764e2f38a6d6f3ec057ac0a516cfbaca0d
James Futhey <kidGodzilla@users.noreply.github.com>
2021-07-31 22:50:02 -0700

    Rough out data aggregation & layout

--- public/src/index.ts
+++ public/src/index.ts
@@ -23,10 +23,106 @@ async function load() {
     wasmUrl.toString()
   );
 
-  const result = await worker.db.query(`select * from visits`);
+  function $(selector) {
+    return document.querySelector(selector) || {}
+  }
+
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  // let statement = `SELECT * FROM visits WHERE date BETWEEN '${ yesterday.toISOString().slice(0,10) }' AND '${ today.toISOString().slice(0,10) }'`;
+  let statement = `SELECT * FROM visits WHERE date = '${ yesterday.toISOString().slice(0,10) }'`;
+  console.log(statement);
+
+  const result = await worker.db.query(statement);
+
+  $('div pre').textContent = JSON.stringify(result, null, 2);
+
+  // Aggregate results
+  let map = {};
+  let countries = {};
+  let pageviews = 0;
+  let visitors = [];
+
+  // console.log(result);
+
+  result.forEach(item => {
+    let d = new Date(item.ts * 1000);
+    let h = d.getHours();
+
+    // Aggregate
+    if (!map[h]) map[h] = 0;
+    map[h]++;
+
+    // Countries
+    if (!countries[item.country_code]) countries[item.country_code] = { visitors: 0 };
+    countries[item.country_code].visitors++;
+
+    // Pageviews & Visitors
+    // @ts-ignore
+    if (!visitors.includes(item.ip)) visitors.push(item.ip);
+    pageviews++;
+  });
+
+  let data = [];
+
+  for (let i = 0; i < 23; i++) {
+    // @ts-ignore
+    data.push({ hour: `${ yesterday.toISOString().slice(0,10) } ${ i }:00`, value: (map[i] || 0) });
+  }
+
+  // console.log(map, data);
 
   // @ts-ignore
-  document.querySelector('div pre').textContent = JSON.stringify(result, null, 2);
+  new Morris.Line({
+    // ID of the element in which to draw the chart.
+    element: 'visits',
+    // Chart data records -- each entry in this array corresponds to a point on
+    // the chart.
+    data: data || [
+      { year: '2008', value: 20 },
+      { year: '2009', value: 10 },
+      { year: '2010', value: 5 },
+      { year: '2011', value: 5 },
+      { year: '2012', value: 20 }
+    ],
+    // The name of the data record attribute that contains x-values.
+    xkey: 'hour',
+    // A list of names of data record attributes that contain y-values.
+    ykeys: ['value'],
+    // Labels for the ykeys -- will be displayed when you hover over the
+    // chart.
+    labels: ['Visits']
+  });
+
+  // SVG Map
+  // @ts-ignore
+  new svgMap({
+    targetElementID: 'svgMap',
+    data: {
+      data: {
+        visitors: {
+          name: 'visitors',
+          format: '{0}',
+          thousandSeparator: ',',
+          // thresholdMax: 50000,
+          // thresholdMin: 1000
+        }
+      },
+      applyData: 'visitors',
+      values: countries || {
+        AF: { visitors: 587 },
+        AL: { visitors: 4583 },
+        DZ: { visitors: 4293 }
+      }
+    }
+  });
+
+  // Pageviews & Visitors
+  $('.visitors').textContent = visitors.length;
+  $('.pageviews').textContent = pageviews;
+
 }
 
 load();
