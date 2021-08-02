575803e47960a8ff2361d638be502a32bc2ede91
James Futhey <kidGodzilla@users.noreply.github.com>
2021-08-01 18:39:08 -0700

    track and send session time, views per session

--- public/a.js
+++ public/a.js
@@ -9,19 +9,31 @@
     function pageview() {
         if (location.pathname === lastPathname) return;
 
-        var isHeadless = !!(window.phantom || window._phantom || window.__nightmare || window.navigator.webdriver || window.Cypress);
+        var viewCount = 1, sessionTime = 0;
+
+        try {
+            sessionTime = (parseInt(sessionStorage.getItem('_sessionTime')) || 1);
+            viewCount = (parseInt(sessionStorage.getItem('_viewCount')) || 0);
+            sessionStorage.setItem('_viewCount', ++viewCount);
+        } catch(e){}
+
         var loadTime = ((window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart) / 1000).toFixed(2);
+        var isHeadless = !!(window.phantom || window._phantom || window.__nightmare || window.navigator.webdriver || window.Cypress);
         var width = window.innerWidth, referrer = document.referrer, href = location.href, host = location.hostname;
         var isBot = /bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent);
         var rand = (Math.random() + 1).toString(36).substring(5);
         var img = document.createElement("img");
 
-        var url = `https://unsimple.b-cdn.net/o.png?host=${ encodeURIComponent(host) }&referrer=${ encodeURIComponent(referrer) }&href=${ encodeURIComponent(href) }&width=${ width }&bot=${ isBot ? 1 : 0 }&headless=${ isHeadless ? 1 : 0 }&load=${ loadTime }&v=${ rand }`;
+        var url = `https://unsimple.b-cdn.net/o.png?host=${ encodeURIComponent(host) }&referrer=${ encodeURIComponent(referrer) }&href=${ encodeURIComponent(href) }&width=${ width }&bot=${ isBot ? 1 : 0 }&headless=${ isHeadless ? 1 : 0 }&load=${ loadTime }&views=${ viewCount }&time=${ sessionTime }&v=${ rand }`;
         lastPathname = location.pathname;
         img.src = url;
 
         document.body.appendChild(img);
     }
+
+    setInterval(function () {
+        try { sessionStorage.setItem('_sessionTime', ((parseInt(sessionStorage.getItem('_sessionTime')) || 1) + 1)) } catch(e){}
+    }, 1000);
 
     documentReady(function() {
         window.addEventListener('visibilitychange', pageview);
