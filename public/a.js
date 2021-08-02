7d46812847b96b22275a8dbe7004552b9e6f2bc6
James Futhey <kidGodzilla@users.noreply.github.com>
2021-08-01 18:18:52 -0700

    capture page load time

--- public/a.js
+++ public/a.js
@@ -1,25 +1,31 @@
 (function () {
     var lastPathname;
 
+    function documentReady(fn) {
+        if (document.readyState === "complete" || document.readyState === "interactive" || (window.performance && !window.performance.timing.domContentLoadedEventEnd)) setTimeout(fn, 1);
+        else document.addEventListener("DOMContentLoaded", fn);
+    }
+
     function pageview() {
         if (location.pathname === lastPathname) return;
 
         var isHeadless = !!(window.phantom || window._phantom || window.__nightmare || window.navigator.webdriver || window.Cypress);
+        var loadTime = ((window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart) / 1000).toFixed(2);
         var width = window.innerWidth, referrer = document.referrer, href = location.href, host = location.hostname;
         var isBot = /bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent);
         var rand = (Math.random() + 1).toString(36).substring(5);
         var img = document.createElement("img");
 
-        var url = `https://unsimple.b-cdn.net/o.png?host=${ encodeURIComponent(host) }&referrer=${ encodeURIComponent(referrer) }&href=${ encodeURIComponent(href) }&width=${ width }&bot=${ isBot ? 1 : 0 }&headless=${ isHeadless ? 1 : 0 }&v=${ rand }`;
+        var url = `https://unsimple.b-cdn.net/o.png?host=${ encodeURIComponent(host) }&referrer=${ encodeURIComponent(referrer) }&href=${ encodeURIComponent(href) }&width=${ width }&bot=${ isBot ? 1 : 0 }&headless=${ isHeadless ? 1 : 0 }&load=${ loadTime }&v=${ rand }`;
         lastPathname = location.pathname;
         img.src = url;
 
         document.body.appendChild(img);
-        // console.log('fetched', url);
-        // fetch(url);
     }
 
-    window.addEventListener("visibilitychange", function() { pageview() });
-    window.addEventListener("popstate", function() { pageview() });
-    pageview();
+    documentReady(function() {
+        window.addEventListener('visibilitychange', pageview);
+        window.addEventListener('popstate', pageview);
+        pageview();
+    });
 })();
